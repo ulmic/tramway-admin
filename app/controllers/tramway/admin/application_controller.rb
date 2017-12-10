7c66d094bba0ba6b65d527023d2441032d8f84f2
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-12-10 04:48:49 +0400

    Fixes

--- app/controllers/tramway/admin/application_controller.rb
+++ app/controllers/tramway/admin/application_controller.rb
@@ -4,18 +4,24 @@ module Tramway
       include AuthManagment
       before_action :authenticate_admin!
       before_action :check_available!
-      before_action :collections_counts
+      before_action :collections_counts, if: :model_given?
 
       protect_from_forgery with: :exception
 
       def check_available!
-        render '/404' unless params[:model].in? ::Tramway::Admin.available_models.map(&:to_s)
+        render '/404' unless model_given?
       end
 
       def collections_counts
         @counts = "#{params[:model]}Decorator".constantize.collections.reduce({}) do |hash, collection|
           hash.merge! collection => params[:model].constantize.send(collection).count
         end
+      end
+
+      private
+
+      def model_given?
+        params[:model].in? ::Tramway::Admin.available_models.map(&:to_s)
       end
     end
   end
--- app/models/tramway/user/user.rb
+++ app/models/tramway/user/user.rb
@@ -1,4 +1,6 @@
 class Tramway::User::User < ::Tramway::Core::ApplicationRecord
+  has_secure_password #FIXME remove repeating from tramway-user
+
   enumerize :role, in: [ :user, :admin ], default: :user
 
   def admin?
